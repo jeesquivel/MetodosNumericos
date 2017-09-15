b833cb48663b023871264266a35ae7083fa20de1
jeesquivel <esquivelsamudio95@gmail.com>
2017-09-15 13:43:43 -0600

    saludar

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,25 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class Controller {
+
+    @FXML
+    Button boton;
+
+    @FXML
+    Label cromo;
+
+
+    public void saludar(){
+
+        cromo.setText("hola ema");
+
+
+
+    }
 
 
 
